3a1ad4febecdbb279d0f528d920315dafaa12e6e
Diogo Henrique Ribeiro <diogo.henrique.ext@olx.com>
2024-07-25 11:33:42 +0100

    wip

--- app/src/main/java/com/motors/testlanguageapp/MainApplication.kt
+++ app/src/main/java/com/motors/testlanguageapp/MainApplication.kt
@@ -1,21 +1,8 @@
 package com.motors.testlanguageapp
 
 import android.app.Application
-import android.content.res.Configuration
-import java.util.Locale
 
 
 class MainApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        setLocaleBasedOnFlavor()
-    }
 
-    private fun setLocaleBasedOnFlavor() {
-        val locale = Locale("es")
-        Locale.setDefault(locale)
-        val config = Configuration()
-        config.setLocale(locale)
-        resources.updateConfiguration(config, resources.displayMetrics)
-    }
 }
