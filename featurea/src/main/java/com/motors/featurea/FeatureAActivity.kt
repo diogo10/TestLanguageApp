8e03f9557303a0acc11c4ff9c7d255114373f414
Diogo Henrique Ribeiro <diogo.henrique.ext@olx.com>
2024-07-25 10:48:43 +0100

    first

---
+++ featurea/src/main/java/com/motors/featurea/FeatureAActivity.kt
@@ -0,0 +1,28 @@
+package com.motors.featurea
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FeatureAActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_feature_a)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, FeatureAActivity::class.java))
+        }
+    }
+}
